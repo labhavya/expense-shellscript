4f6ec4a8a8287b1ef7c7f278cadfb49c8d79ba1a
Bhavya <bhavya571@gmail.com>
2025-01-03 17:41:13 +0530

    install mysql

--- backend.sh
+++ backend.sh
@@ -56,10 +56,10 @@ VALIDATE $? "creating app directory"
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "downlaoding backend" 
 
-cd /app
+cd /app 
 
 rm -rf /app/*
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip backend"
 
 npm install &>>$LOG_FILE_NAME
