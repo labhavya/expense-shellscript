6647823321861ddcfb7e8689b8b37a107083d3c9
Bhavya <bhavya571@gmail.com>
2025-01-03 13:13:51 +0530

    install mysql

---
+++ mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#!/bin/bash
+USER=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT(){
+            if [ $USER -ne 0 ]
+            then 
+                echo "ERROR:: user doens't have permission to install"
+                exit 1
+            fi 
+}
+VALIDATE() 
+{
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2....$G success $N"
+        
+    else 
+        echo -e "$2....$R failed $N"
+        exit 1
+    fi
+}
+CHECK_ROOT
+
+dnf isntall mysql-server -y &>>$LOG_FILE_NAME
+VALIDATE $? "mysql install is"
+
+systemctl enable mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "service enable is"
+
+systemctl start mysqld &>>$LOG_FILE_NAME
+VALIDATE $? "service start is"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "setting root password"
